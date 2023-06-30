5ca106f831331f649fad7bba5e01d9d4eb9acb2f
harimu63 <137975117+harimu63@users.noreply.github.com>
2023-06-30 16:33:53 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -296,9 +296,7 @@ clear
                     echo -e "${Kcyan}│${NC}${Kbiru} [•4]${NC}📨 ${white} TROJAN-GO${NC}  " "${Kbiru}[•9]${NC}📨 ${white} SET PASW VPS${NC} "   "${Kcyan}│${NC}"
                     echo -e "${Kcyan}│${NC}${Kbiru} [•5]${NC}📨 ${white} TROJAN-WS${NC}  " "${Kbiru}[10]${NC}📨 ${white} MENU SETINGS${NC} "   "${Kcyan}│${NC}"
                     echo -e "${Kcyan}└───────────────────────────────────────────┘"   
-                    echo -e "${green}                                                                         [ 11 ]                       "
-                    echo -e "${yellow}                                                              UPDATE MENU               "
-                    echo -e "                                                      ----------------------------------------------------------------- " | lolcat
+                    echo -e "----------------------------------------------------------------- " | lolcat
 echo -e ""
 echo -e   "${kuning}"
 read -p " select menu [ 1 / 10 ] >>>    "  opt
