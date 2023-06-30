c3a980549d604d0dd05f9173828c3b9f6b0e174e
harimu63 <137975117+harimu63@users.noreply.github.com>
2023-06-30 12:41:22 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -411,28 +411,28 @@ apt autoremove -y >/dev/null 2>&1
 # finishing
 cd
 chown -R www-data:www-data /home/vps/public_html
-sleep 1
+sleep 2
 echo -e "$yell[SERVICE]$NC Restart All service SSH & OVPN"
 /etc/init.d/nginx restart >/dev/null 2>&1
-sleep 1
+sleep 2
 echo -e "[ ${green}ok${NC} ] Restarting nginx"
 /etc/init.d/openvpn restart >/dev/null 2>&1
-sleep 1
+sleep 2
 echo -e "[ ${green}ok${NC} ] Restarting cron "
 /etc/init.d/ssh restart >/dev/null 2>&1
-sleep 1
+sleep 2
 echo -e "[ ${green}ok${NC} ] Restarting ssh "
 /etc/init.d/dropbear restart >/dev/null 2>&1
-sleep 1
+sleep 2
 echo -e "[ ${green}ok${NC} ] Restarting dropbear "
 /etc/init.d/fail2ban restart >/dev/null 2>&1
-sleep 1
+sleep 2
 echo -e "[ ${green}ok${NC} ] Restarting fail2ban "
 /etc/init.d/stunnel4 restart >/dev/null 2>&1
-sleep 1
+sleep 2
 echo -e "[ ${green}ok${NC} ] Restarting stunnel4 "
 /etc/init.d/vnstat restart >/dev/null 2>&1
-sleep 1
+sleep 2
 echo -e "[ ${green}ok${NC} ] Restarting vnstat "
 /etc/init.d/squid restart >/dev/null 2>&1
 
@@ -449,10 +449,10 @@ history -c
 echo "unset HISTFILE" >> /etc/profile
 
 
-rm -f /root/key.pem
-rm -f /root/cert.pem
-rm -f /root/ssh-vpn.sh
-rm -f /root/bbr.sh
+#rm -f /root/key.pem
+#rm -f /root/cert.pem
+#rm -f /root/ssh-vpn.sh
+#rm -f /root/bbr.sh
 
 # finihsing
 clear
